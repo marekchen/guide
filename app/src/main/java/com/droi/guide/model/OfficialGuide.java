e3384246955e09536972e603e90d6533389c4ae6
chenpei <marek@163.com>
2016-09-06 21:27:25 +0800

    update

--- app/src/main/java/com/droi/guide/model/OfficialGuide.java
+++ app/src/main/java/com/droi/guide/model/OfficialGuide.java
@@ -1,14 +1,18 @@
 package com.droi.guide.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.droi.sdk.core.DroiExpose;
 import com.droi.sdk.core.DroiObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by chenpei on 16/9/3.
  */
-public class OfficialGuide extends DroiObject{
+public class OfficialGuide extends DroiObject {
     @DroiExpose
     public String location;
     @DroiExpose
@@ -18,5 +22,45 @@ public class OfficialGuide extends DroiObject{
     @DroiExpose
     public String brief;
     @DroiExpose
-    public List<OfficialGuideStep> steps;
+    public ArrayList<OfficialGuideStep> steps;
+    @DroiExpose
+    public int favoriteNum;
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(location);
+        parcel.writeString(category);
+        parcel.writeString(title);
+        parcel.writeString(brief);
+        parcel.writeList(steps);
+        parcel.writeInt(favoriteNum);
+    }
+
+    @Override
+    public int describeContents() {
+        return super.describeContents();
+    }
+
+    public static final Parcelable.Creator<OfficialGuide> CREATOR = new Parcelable.Creator<OfficialGuide>() {
+        public OfficialGuide createFromParcel(Parcel in) {
+            return new OfficialGuide(in);
+        }
+
+        public OfficialGuide[] newArray(int size) {
+            return new OfficialGuide[size];
+        }
+    };
+
+    private OfficialGuide(Parcel in) {
+        location = in.readString();
+        category = in.readString();
+        title = in.readString();
+        brief = in.readString();
+        in.readList(steps, OfficialGuideStep.class.getClassLoader());
+        favoriteNum = in.readInt();
+    }
+
+    public OfficialGuide() {
+
+    }
 }
